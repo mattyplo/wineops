2cca5116da8b07527f02fc4c62ccf04c6ae0e7fa
mattyplo <matthew.ploszaj@seattlecentral.edu>
2026-06-28 18:57:41 -0700

    Configure frontend API URL

--- frontend/src/api/readings.ts
+++ frontend/src/api/readings.ts
@@ -1,6 +1,6 @@
 import type { Reading } from "../types/reading";
 
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 
 export async function getLatestReadings(): Promise<Reading[]> {
